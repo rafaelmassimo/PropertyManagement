94466bf5712b1ce7266647d2d6124c8951683bcd
Rafael Massimo <rafael_massimo@hotmail.com>
2024-06-03 18:55:45 -0700

    create model and route for send message

---
+++ app/api/messages/route.js
@@ -0,0 +1,48 @@
+import connectDB from '@/config/database';
+import Message from '@/models/Message';
+import { getSessionUser } from '@/utils/getSessionUser';
+
+export const dynamic = 'force dynamic';
+
+//POST /api/messages
+export const POST = async (request) => {
+	try {
+		await connectDB();
+		const { name, email, phone, property, recipient, message } = await request.json();
+
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.user) {
+			return new Response(
+				JSON.stringify({ message: 'You must be logged in to sent the message' }),
+				{ status: 401 },
+			);
+		}
+
+		const { user } = sessionUser;
+
+		//Cannot send message to self
+		if (user.id === recipient) {
+			return new Response(JSON.stringify({ message: 'Cannot send message to yourself' }), {
+				status: 400,
+			});
+		}
+
+		const NewMessage = new Message({
+			sender: user.id,
+			recipient,
+			property,
+			name,
+			email,
+			phone,
+			body: message,
+		});
+
+		await NewMessage.save(); //Here you are saving the message to the database following the schema you created in the model
+
+		return new Response(JSON.stringify({ message: 'Message sent successfully' }, { status: 200 }));
+	} catch (error) {
+		console.log(error);
+		return new Response('Something went wrong', { status: 500 });
+	}
+};
